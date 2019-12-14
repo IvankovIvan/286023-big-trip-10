d17f10b65952504e0e92152e70d4aa5383b9a8ab
Ivankov <ivan20481@gmail.com>
2019-12-14 12:37:16 +0300

    item to class

--- src/components/itemDay.js
+++ src/components/itemDay.js
@@ -1,14 +1,12 @@
 import {MonthNames} from "../const";
 import {getDateFormatUS} from "../utilsDate";
-import {createItemTripTemplate} from './item.js';
 import {createElement} from "../utils";
 
-export const createItemDayTemplate = (trips) => {
+const createItemDayTemplate = (trips) => {
   const {schedule} = trips[0];
   const day = schedule.timeStart.getDate();
   const month = MonthNames[schedule.timeStart.getMonth()];
   const year = schedule.timeStart.getFullYear().toString().slice(-2);
-  const tripsDay = trips.map((trip) => createItemTripTemplate(trip)).join(`\n`);
   return (
     `<li class="trip-days__item  day">
       <div class="day__info">
@@ -18,7 +16,6 @@ export const createItemDayTemplate = (trips) => {
         >${month} ${year}</time>
       </div>
         <ul class="trip-events__list">
-          ${tripsDay}
         </ul>
     </li>`
   );
--- src/main.js
+++ src/main.js
@@ -4,19 +4,13 @@ import MenuComponent from './components/menu.js';
 import FilterComponent from './components/filter.js';
 import SortComponent from './components/sort.js';
 import EditComponent from './components/edit.js';
-// import {createItemsTemplate} from './components/items.js';
 import ItemsComponent from "./components/items";
-// import {createItemDayTemplate} from './components/itemDay.js';
 import ItemDayComponent from './components/itemDay.js';
-
+import ItemComponent from './components/item.js';
 import {generateTrips} from './mock/trips.js';
 import {Menu, Filters, TRIP_POINT_VIEW} from './const.js';
 import {sortDateArray} from './utilsDate.js';
 import {renderElement, RenderPosition} from './utils.js';
-
-const render = (container, template, place = `beforeend`) => {
-  container.insertAdjacentHTML(place, template);
-};
 
 const createTripList = (trips, element) => {
   const tripList = trips.slice(1, TRIP_POINT_VIEW);
@@ -26,8 +20,14 @@ const createTripList = (trips, element) => {
     const tripListFromDate = tripList.filter((item) => {
       return Date.parse(item.schedule.date) === date;
     });
-    // render(element, createItemDayTemplate(tripListFromDate));
-    renderElement(element, new ItemDayComponent(tripListFromDate).getElement());
+    const itemDayComponent = new ItemDayComponent(tripListFromDate)
+      .getElement();
+    renderElement(element, itemDayComponent);
+    const tripEventsList = itemDayComponent
+      .querySelector(`.trip-events__list`);
+    tripListFromDate.forEach((trip) => {
+      renderElement(tripEventsList, new ItemComponent(trip).getElement());
+    });
   });
 };
 
@@ -36,26 +36,18 @@ const tripArray = generateTrips().sort(sortDateArray);
 const siteMainElement = document.querySelector(`.page-body`);
 const siteInfoElement = siteMainElement.querySelector(`.trip-main__trip-info`);
 
-// render(siteInfoElement, createInfoTemplate(tripArray), `afterbegin`);
 renderElement(siteInfoElement, new InfoComponent(tripArray).getElement(),
     RenderPosition.AFTERBEGIN);
-// render(siteInfoElement, createPriceTemplate(tripArray));
 renderElement(siteInfoElement, new PriceComponent(tripArray).getElement());
 
 const siteControlElement = siteMainElement
   .querySelector(`.trip-main__trip-controls`);
 renderElement(siteControlElement.firstElementChild, new MenuComponent(Menu).getElement(),
     RenderPosition.AFTERNODE);
-// render(siteControlElement.firstElementChild, createMenuTemplate(Menu),
-//     `afterend`);
-// render(siteControlElement, createFilterTemplate(Filters));
 renderElement(siteControlElement, new FilterComponent(Filters).getElement());
 
 const siteEventsElement = siteMainElement.querySelector(`.trip-events`);
-// render(siteEventsElement, createSortTemplate());
 renderElement(siteEventsElement, new SortComponent().getElement());
-// render(siteEventsElement, createEditTemplate(tripArray[0]));
 renderElement(siteEventsElement, new EditComponent(tripArray[0]).getElement());
-// render(siteEventsElement, createItemsTemplate());
 renderElement(siteEventsElement, new ItemsComponent().getElement());
 createTripList(tripArray, siteEventsElement.lastElementChild);
